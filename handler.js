48f1e16324b7696b2478416a879d5341e34f5cb0
Charlie McClung <charlie@cmr1.com>
2018-11-27 13:48:24 -0700

    Basic working message response

--- handler.js
+++ handler.js
@@ -1,14 +1,12 @@
-'use strict';
+const Sentencer = require('sentencer')
 
-module.exports.hello = async (event, context) => {
+module.exports.message = async (event, context) => {
+  const message = Sentencer.make("This sentence has {{ a_noun }} and {{ an_adjective }} {{ noun }} in it.")
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message
     }),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  }
+}
